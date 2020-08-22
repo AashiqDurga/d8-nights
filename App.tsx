fff9316e4473f742f769b1766f6b5a520331eca0
Aashiq Durga <aashiqdurga@gmail.com>
2020-08-22 17:05:34 +1200

    fixed themeing

--- App.tsx
+++ App.tsx
@@ -3,17 +3,18 @@ import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry, Layout, Text } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { AppNavigator } from './navigation/app-navigator';
+import { default as theme } from './theme.json';
 
-const HomeScreen = () => (
-  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text category='h1'>HOME</Text>
-  </Layout>
-);
+// const HomeScreen = () => (
+//   <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+//     <Text category='h1'>HOME</Text>
+//   </Layout>
+// );
 
 export default () => (
   <>
     <IconRegistry icons={EvaIconsPack} />
-    <ApplicationProvider {...eva} theme={eva.light}>
+    <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
     <AppNavigator/>
     </ApplicationProvider>
   </>
