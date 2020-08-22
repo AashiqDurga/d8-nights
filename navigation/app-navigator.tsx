c1c58930cf554cc6decad5aa2360570a6b4a4497
Aashiq Durga <aashiqdurga@gmail.com>
2020-08-22 17:58:43 +1200

    add mesages tab ans profile settings

--- navigation/app-navigator.tsx
+++ navigation/app-navigator.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { HomeScreen } from "../screens/home";
-import { ProfileScreen } from "../screens/profile";
+import Profile  from "../screens/profile/index";
 import {
   Layout,
   Text,
@@ -15,12 +14,14 @@ import {
 const bottomTab = createBottomTabNavigator();
 
 const HomeIcon = (props: any) => <Icon {...props} name="home-outline" />;
+const ChatIcon = (props: any) => <Icon {...props} name="message-square-outline" />;
 const ProfileIcon = (props: any) => <Icon {...props} name="person-outline" />;
 const BottomTabBar = ({ navigation, state }: any) => (
   <BottomNavigation
     selectedIndex={state.index}
     onSelect={(index) => navigation.navigate(state.routeNames[index])}
   >
+    <BottomNavigationTab icon={ChatIcon} />
     <BottomNavigationTab icon={HomeIcon} />
     <BottomNavigationTab icon={ProfileIcon} />
   </BottomNavigation>
@@ -28,8 +29,9 @@ const BottomTabBar = ({ navigation, state }: any) => (
 
 const TabNavigator = () => (
   <bottomTab.Navigator tabBar={(props) => <BottomTabBar {...props} />}>
+    <bottomTab.Screen name="Dates" component={HomeScreen} />
     <bottomTab.Screen name="Home" component={HomeScreen} />
-    <bottomTab.Screen name="Profile" component={ProfileScreen} />
+    <bottomTab.Screen name="Profile" component={Profile} />
   </bottomTab.Navigator>
 );
 export const AppNavigator = () => (
