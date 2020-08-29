017f7cb6f525a0f0060cacdcae49b1ffefe4e4f5
Aashiq Durga <aashiqdurga@gmail.com>
2020-08-29 14:49:34 +1200

    made confirm page pretty

--- screens/auth/confirm/confirm.tsx
+++ screens/auth/confirm/confirm.tsx
@@ -1,16 +1,11 @@
 import React, { useState } from "react";
-import { SafeAreaView } from "react-native";
-import {
-  Text,
-  Divider,
-  Layout,
-  TopNavigation,
-  Input,
-  Button,
-} from "@ui-kitten/components";
+import { StyleSheet, View, Alert } from "react-native";
+import { Button, Input, Text } from "@ui-kitten/components/ui";
+import { ImageOverlay } from "../sign-in/extra/image-overlay.component";
+import { KeyboardAvoidingView } from "../sign-in/extra/3rd-party";
 import { Auth } from "aws-amplify";
 
-const confirm = ({ route, navigation }) => {
+const Confirm =({ route, navigation }): React.ReactElement => {
   const [code, setCode] = useState<string>("");
   const { email } = route.params;
 
@@ -21,25 +16,62 @@ const confirm = ({ route, navigation }) => {
       navigation && navigation.navigate("Home", { screen: "Home" });
     } catch (error) {
       console.log("error confirming sign up", error);
+      Alert.alert("Is that the code we sent you? 👀", error.message);
     }
   }
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <TopNavigation title="D8 Nights" alignment="center" />
-      <Divider />
-      <Layout
-        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      <ImageOverlay
+        style={styles.container}
+        source={require("../sign-in/assets/image-background.jpg")}
       >
-        <Text category="h1">D8's</Text>
-        <Input onChangeText={setCode}></Input>
+        <View style={styles.headerContainer}>
+          <Text category="h1" status="control">
+            Almost there 👏
+          </Text>
+          <Text style={styles.confirmLabel} category="s1" status="control">
+            Enter the confirmation code we emailed to you 🕵🏽‍♀️
+          </Text>
+        </View>
+        <View style={styles.formContainer}>
+          <Input
+            status="control"
+            placeholder="Confirmation Code"
+            keyboardType="numeric"
+            value={code}
+            onChangeText={setCode}
+          />
+        </View>
         <Button
+          style={styles.confirmButton}
+          size="giant"
           onPress={onConfirmSignUpButtonPress}
-          accessibilityLabel="Confirm"
         >
           Confirm
         </Button>
-      </Layout>
-    </SafeAreaView>
+      </ImageOverlay>
   );
 };
-export default confirm;
+
+export default Confirm;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerContainer: {
+    minHeight: 216,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  formContainer: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  confirmLabel: {
+    marginTop: 16,
+  },
+  confirmButton: {
+    marginHorizontal: 16,
+  },
+});
